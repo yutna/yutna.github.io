021c6785f4442ae3efb75390f863bc76a40f27ef
Yutthana Siphuengchai <yutthana.s@outlook.com>
2023-12-09 23:46:16 +0700

    Setup Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  integrations: [tailwind()],
   site: "https://yutna.github.io",
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Kanit", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+};
