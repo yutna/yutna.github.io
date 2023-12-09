b85288bdd09e931ed13ffb94ed8f53137319f02d
Yutthana Siphuengchai <yutthana.s@outlook.com>
2023-12-09 19:35:56 +0700

    Remove all generated content and upgrade Astro to version 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
   site: "https://yutna.github.io",
 });
