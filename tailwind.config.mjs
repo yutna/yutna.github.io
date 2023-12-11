750db5af1db0b56f85f11925ac945d6822b5f8d1
Yutthana Siphuengchai <32380889+yutna@users.noreply.github.com>
2023-12-12 00:59:50 +0700

    Remove fonts and pre-configured colors (#15)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -20,22 +20,6 @@ export default {
       xl: "1280px",
     },
     extend: {
-      backgroundImage: {
-        "gradient-dark-silver":
-          "linear-gradient(112deg, #171717 3.96%, rgba(23, 23, 23, 0.4) 119.41%)",
-        "gradient-silver":
-          "linear-gradient(112deg, #f9f9f9 3.96%, rgba(133, 138, 146, 0) 119.41%)",
-        "gradient-violet-neon":
-          "linear-gradient(91.92deg, #7852fe 5.45%, #8a09cd 47.89%, #e21bda 88.9%)",
-      },
-      boxShadow: {
-        "violet-neon":
-          "0px 0px 10px rgba(151, 71, 255, 0.8), inset 0px 6.91px 25.35px #9747ff, inset 0px -7px 9.22px rgba(234, 218, 255, 0.25)",
-        "violet-neon-md":
-          "0px 0px 29.9px rgba(151, 71, 255, 0.8), inset 0px 6.91px 25.35px #9747ff, inset 0px -7px 9.22px rgba(234, 218, 255, 0.25)",
-        "violet-neon-lg":
-          "4px 0px 20px #9747ff, 0px 0px 29.9px rgba(151, 71, 255, 0.8), inset 0px 6.91px 25.35px #9747ff, inset 0px -7px 9.22px rgba(234, 218, 255, 0.25)",
-      },
       colors: {
         "cultured": "#f5f5f0",
         "eerie-black": "#191919",
@@ -44,7 +28,7 @@ export default {
         "milk": "#fafaf5",
       },
       fontFamily: {
-        mono: ["Noto Sans Mono Variable", ...defaultTheme.fontFamily.mono],
+        sans: ["Kanit", ...defaultTheme.fontFamily.sans],
       },
     },
   },
