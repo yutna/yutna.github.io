be43848002e061adeeab1cbd190dedaba972135e
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-01-08 04:39:02 +0700

    Refactor navigation component

---
+++ src/config/menus.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Blog",
+    url: "/blog",
+  },
+  {
+    name: "Projects",
+    url: "/projects",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+];
